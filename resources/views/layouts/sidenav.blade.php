ab0fa3ead3ff6d9e36b816dcf032df4743dc78f3
Ricky Millang <rickymillang@gmail.com>
2018-01-26 06:25:56 +0800

    merge kurt update

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -17,18 +17,18 @@
             <li role="presentation"><a href="/packages"><span class="fa fa-window-restore"></span> Packages</a></li>
             <li role="presentation"><a href="/invoices"><span class="fa fa-file-text-o"></span> Invoices</a></li>
         @endif
-        @role('superadmin')
+         @role('superadmin')
             <li role="presentation"><a href="/establishment_types"><span class="fa fa-file-text-o"></span> Establishment Types</a></li>
             <li role="presentation"><a href="/customers"><span class="fa fa-file-text-o"></span> Customers</a></li>
             <li role="presentation"><a href="/administrators"><span class="fa fa-file-text-o"></span> Administrators</a></li>
-        @endrole
+         @endrole
         <li role="presentation"><a href="/payments"><span class="fa fa-credit-card"></span> Payments</a></li>
         <li role="presentation"><a href="#"><span class="fa fa-envelope"></span> Messages</a></li>
     </ul>
 
-    @if(!auth()->user()->hasRole('establishment.admin'))
+    @role('superadmin' || 'establisment.admin')
 
         <a href="/establishments" class="btn btn-success btn-block"><span class="fa fa-building"></span> Register Establishment Here</a>
-    @endif
+    @endrole
 </div>
 
