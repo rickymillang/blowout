86696516ade46d1077cf0902f8ead3b73dd7d994
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-02-01 11:28:17 +0800

    Remove unnecessary declaration of table variable

--- app/Service.php
+++ app/Service.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    protected $table = 'services';
-
     protected $fillable = ['establishment_id', 'name', 'description', 'price'];
 
     public function packages()
