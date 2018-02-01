cf23857e3b47daa6ed78686aef594da4d6f4a08a
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-02-01 10:19:30 +0800

    Create migration for packages

---
+++ database/migrations/2018_01_31_181454_create_packageables_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackageablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packageables', function (Blueprint $table) {
+            $table->integer('package_id');
+            $table->integer('packageable_id');
+            $table->string('packageable_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packageables');
+    }
+}
