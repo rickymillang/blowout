9eaa6cf907a2d24eca29d1d6ed526bc580614a4d
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-02-02 03:21:09 +0800

    Remove unnecessary container

--- resources/views/establishment_types/create.blade.php
+++ resources/views/establishment_types/create.blade.php
@@ -3,14 +3,10 @@
 @section('heading', 'Add Establishment Type')
 
 @section('content')
-	<div class="container" style="margin-bottom:10px;">
-        <div class="row">
-            <div class="col-sm-12">
-                <a href="/establishment_types" class="btn btn-success">View Establishment Types</a>
-            </div>
-        </div>
-    </div>
+    <a href="/establishment_types" class="btn btn-success">View Establishment Types</a>
+
     <hr>
+
     <form class="form-horizontal" method="POST" action="/establishment_types">
 		{{ csrf_field() }}
 
@@ -20,7 +16,7 @@
 				<input type="text" class="form-control" name="name">
 			</div>
 		</div>
-		
+
 		<div class="form-group">
 			<div class="col-md-6 col-md-offset-4">
 				<button class="btn btn-primary">Submit</button>
