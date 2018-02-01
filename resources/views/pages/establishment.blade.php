115e040c2fb888e593c2a80f8e0d190f3b7b726a
rickymillang <rickymillang@gmail.com>
2018-02-01 11:19:18 +0800

    modify establishment blade

--- resources/views/pages/establishment.blade.php
+++ resources/views/pages/establishment.blade.php
@@ -76,7 +76,7 @@
 				<div class="col-md-12 col-md-offset-0 text-center">
 					<div class="row row-mt-15em">
 						<div class="col-md-12 mt-text animate-box" data-animate-effect="fadeInUp">
-							<h1>Destination</h1>
+							<h1>Establishments</h1>
 						</div>
 
 					</div>
