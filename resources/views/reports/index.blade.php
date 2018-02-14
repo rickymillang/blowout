4402677d13952bfde32d99b35d94d9dc08927a71
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-02-15 01:35:27 +0800

    Add index view fore reports

---
+++ resources/views/reports/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('heading', 'View Reports')
+
+@section('content')
+    @if(auth()->user()->hasRole('establishment.admin') || auth()->user()->hasRole('customer'))
+        <a href="/reports/create" class="btn btn-success">Submit Report</a>
+
+        <hr>
+    @endif
+    <table class="table table-striped table-hover" id="table">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Message</th>
+                <th>Reporter</th>
+                <th>Reported At</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($reports as $report)
+                <tr>
+                    <td>{{ $report->title }}</td>
+                    <td>{{ $report->message }}</td>
+                    <td>{{ $report->user->name }}</td>
+                    <td>{{ $report->updated_at->toFormattedDateString() }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
+
+@section('custom_css')
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.css"/>
+@endsection
+
+@section('custom_js')
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.js"></script>
+<script>
+    $(document).ready(function() {
+    $('#table').DataTable();
+} );
+</script>
+@endsection
