9249f279549b894b4d01ec6e5d31657308764652
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-02-14 23:26:47 +0800

    Add view for agreement

---
+++ resources/views/agreements/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('heading', 'Terms and Conditions')
+
+@section('content')
+    <h4>The following are the terms and conditions:</h4>
+    <ol>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, libero nemo atque. Atque debitis facere accusamus molestias. Quidem eius dolorem quis tenetur voluptas voluptatum sint saepe quasi, blanditiis, animi fugit!</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur modi sapiente perspiciatis quos exercitationem! Modi iure fugiat quia, laborum sapiente veniam architecto debitis voluptatibus dicta temporibus voluptas. Veniam, placeat, soluta!</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quo, molestiae molestias iure doloremque ratione facere dolorem cupiditate suscipit similique tenetur optio. Culpa beatae hic eos. Iste aliquid officia ullam</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt est maxime odio labore eveniet, accusantium placeat aliquid at inventore officia, ducimus modi aperiam praesentium quis sunt consequuntur, sed voluptatum iste.</li>
+    </ol>
+@endsection
