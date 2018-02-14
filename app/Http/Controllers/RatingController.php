398c8d17c4e181e1cc45f19a0d1e5797d60fc4ca
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-02-14 23:49:08 +0800

    Add error trapping if there are no ratings

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -17,7 +17,12 @@ class RatingController extends Controller
     	$three_star_ratings = Rating::where('rating', 3)->where('establishment_id', auth()->user()->establishment->id)->count();
     	$four_star_ratings = Rating::where('rating', 4)->where('establishment_id', auth()->user()->establishment->id)->count();
     	$five_star_ratings = Rating::where('rating', 5)->where('establishment_id', auth()->user()->establishment->id)->count();
-    	$average_rating = $sum_ratings / $total_ratings_count;
+    	if ($total_ratings_count) {
+    		$average_rating = $sum_ratings / $total_ratings_count;
+    	} else {
+    		$average_rating = 0;
+    	}
+
 
     	return view('ratings.index', compact('ratings', 'sum_ratings', 'average_rating', 'total_ratings_count', 'one_star_ratings', 'two_star_ratings', 'three_star_ratings', 'four_star_ratings', 'five_star_ratings'));
     }
--- resources/views/ratings/index.blade.php
+++ resources/views/ratings/index.blade.php
@@ -3,49 +3,53 @@
 @section('heading', 'View Ratings')
 
 @section('content')
-    <h3>
-        Ratings count: {{ $total_ratings_count }}
-    </h3>
-    <h3>
-        Average rating: {{ round($average_rating, 2) }} <span class="fa fa-star"></span>
-    </h3>
+    @if($total_ratings_count)
+        <h3>
+            Ratings count: {{ $total_ratings_count }}
+        </h3>
+        <h3>
+            Average rating: {{ round($average_rating, 2) }} <span class="fa fa-star"></span>
+        </h3>
 
-    1 <span class="fa fa-star"></span>
-    <div class="progress" style="margin-bottom:0;">
-      <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($one_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($one_star_ratings / $total_ratings_count) * 100, 2) }}%;">
-        {{ $one_star_ratings }}
-      </div>
-    </div>
-    2 <span class="fa fa-star"></span>
-    <div class="progress">
-      <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($two_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($two_star_ratings / $total_ratings_count) * 100, 2) }}%;">
-        {{ round(($two_star_ratings / $total_ratings_count) * 100, 2) }}
-      </div>
-    </div>
-    3 <span class="fa fa-star"></span>
-    <div class="progress">
-      <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($three_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($three_star_ratings / $total_ratings_count) * 100, 2) }}%;">
-        {{ round(($three_star_ratings / $total_ratings_count) * 100, 2) }}
-      </div>
-    </div>
-    4 <span class="fa fa-star"></span>
-    <div class="progress">
-      <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($four_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($four_star_ratings / $total_ratings_count) * 100, 2) }}%;">
-        {{ round(($four_star_ratings / $total_ratings_count) * 100, 2) }}
-      </div>
-    </div>
-    5 <span class="fa fa-star"></span>
-    <div class="progress">
-      <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($five_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($five_star_ratings / $total_ratings_count) * 100, 2) }}%;">
-        {{ round(($five_star_ratings / $total_ratings_count) * 100, 2) }}
-      </div>
-    </div>
-    <hr />
-    @foreach($ratings as $rating)
-        <h4>
-            {{ $rating->user->name }} rated {{ $rating->rating }} <span class="fa fa-star"></span> on {{ $rating->updated_at->toFormattedDateString() }}
-        </h4>
-    @endforeach
+        1 <span class="fa fa-star"></span>
+        <div class="progress" style="margin-bottom:0;">
+          <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($one_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($one_star_ratings / $total_ratings_count) * 100, 2) }}%;">
+            {{ $one_star_ratings }}
+          </div>
+        </div>
+        2 <span class="fa fa-star"></span>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($two_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($two_star_ratings / $total_ratings_count) * 100, 2) }}%;">
+            {{ round(($two_star_ratings / $total_ratings_count) * 100, 2) }}
+          </div>
+        </div>
+        3 <span class="fa fa-star"></span>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($three_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($three_star_ratings / $total_ratings_count) * 100, 2) }}%;">
+            {{ round(($three_star_ratings / $total_ratings_count) * 100, 2) }}
+          </div>
+        </div>
+        4 <span class="fa fa-star"></span>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($four_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($four_star_ratings / $total_ratings_count) * 100, 2) }}%;">
+            {{ round(($four_star_ratings / $total_ratings_count) * 100, 2) }}
+          </div>
+        </div>
+        5 <span class="fa fa-star"></span>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="{{ round(($five_star_ratings / $total_ratings_count) * 100, 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ round(($five_star_ratings / $total_ratings_count) * 100, 2) }}%;">
+            {{ round(($five_star_ratings / $total_ratings_count) * 100, 2) }}
+          </div>
+        </div>
+        <hr />
+        @foreach($ratings as $rating)
+            <h4>
+                {{ $rating->user->name }} rated {{ $rating->rating }} <span class="fa fa-star"></span> on {{ $rating->updated_at->toFormattedDateString() }}
+            </h4>
+        @endforeach
+    @else
+        <h5><em>There are no ratings</em></h5>
+    @endif
 @endsection
 
 @section('custom_css')
