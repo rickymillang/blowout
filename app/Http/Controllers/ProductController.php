c3b506c23c672f8c6366364af77be72b1505f11a
rickymillang <rickymillang@gmail.com>
2018-02-20 07:53:11 +0800

    change page redirect after establishment registration

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
      public function __construct(){
         $this->middleware(function ($request, $next) {
-            dd(auth()->user()->establishment);
+
             $this->establishment_id = auth()->user()->establishment->id;
 
 
