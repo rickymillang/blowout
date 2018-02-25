ecd7d75739235fe3d86be35816e378ec18459c02
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-02-26 03:18:33 +0800

    Add calendar dynamic data

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
 use Illuminate\Http\Request;
 
 class CalendarController extends Controller
 {
     public function index()
     {
-    	return view('calendars.index');
+    	$orders = Order::all();
+    	return view('calendars.index', compact('orders'));
     }
 }
