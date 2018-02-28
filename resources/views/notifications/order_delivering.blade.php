f90553a0734c57ac4675529952461c555810112a
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-02-28 20:48:03 +0800

    Add order notifications view

---
+++ resources/views/notifications/order_delivering.blade.php
@@ -0,0 +1,6 @@
+<a href="/dashboard">
+    <div>
+        <i class="fa fa-envelope fa-fw"></i> {{ $notification->data['message'] }}: <strong>{{ $notification->data['order'] }}
+        <span class="pull-right text-muted small">{{ $notification->created_at->diffForHumans() }}</span>
+    </div>
+</a>
