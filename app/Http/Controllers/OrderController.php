85507d564c8d703c412f7af3688d9bfb6b9506bf
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-02-28 20:09:30 +0800

    Add edit and update method

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,4 +14,44 @@ class OrderController extends Controller
 
     	return view('orders.index', compact('orders'));
     }
+
+    public function edit($id)
+    {
+    	$order = Order::find($id);
+
+    	return view('orders.edit', compact('order'));
+    }
+
+    public function update(Request $request, $id)
+    {
+    	$order = Order::where('id', $id)->where('establishment_id', auth()->user()->establishment->id)->first();
+
+    	$status = $request->status;
+
+    	if ($order) {
+    		$order->status = $status;
+
+    		$order->save();
+
+    		if ($status == 1) {
+    			$message = 'Congratulations, ' . $order->users->name . '! Your order has been approved';
+    		} else if ($status == 3) {
+    			$message = 'Hi, ' . $order->users->name . '. Your order has been completed';
+    		} else if ($status == 4) {
+    			$message = 'Sorry, ' . $order->users->name . '. Your order has been declined';
+    		} else if ($status == 5) {
+    			$message = 'Hi, ' . $order->users->name . '. Your order is now being delivered';
+    		}
+
+    		// Semaphore::send($order->users->contact, $message);
+
+    		session()->flash('message', 'You have successfully changed the order status');
+
+    		return redirect()->back();
+    	} else {
+    		session()->flash('error', 'Order could not be found');
+
+    		return redirect()->back();
+    	}
+    }
 }
