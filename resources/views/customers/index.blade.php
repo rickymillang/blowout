7fb7fe3813cbc3d50aeb928bb2ada74b216ee69f
Kurt Dacudag <kurtperaldacudag@gmail.com>
2018-03-05 02:40:04 +0800

    Remove customer delete

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -14,7 +14,6 @@
                         <th>Image</th>
                         <th>Name</th>
                         <th>Email</th>
-                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -23,35 +22,6 @@
                             <td><img src="{{ asset($customer->avatar) }}"  style="max-width:100px;max-height:100px;"></td>
                             <td>{{ $customer->name }}</td>
                             <td>{{ $customer->email }}</td>
-                            <td>
-                            	<button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#customer{{ $customer->id }}">Delete</button>
-                            	<div id="customer{{ $customer->id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="delete-user">
-                            		<div class="modal-dialog modal-sm" role="document">
-                            			<div class="modal-content">
-                            				<div class="modal-header">
-                            					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            				</div>
-                            				<div class="modal-body">
-                            					Are you sure you want to delete this customer?
-                            				</div>
-                            				<div class="modal-footer">
-                            					<button type="button" class="btn btn-default btn-xs" data-dismiss="modal">Close</button>
-
-                            					<a href="/customer/{{ $customer->id }}" class="btn btn-danger btn-xs"
-                            					    onclick="event.preventDefault();
-                            					             document.getElementById('delete-customer{{ $customer->id }}').submit();">
-                            					    Delete
-                            					</a>
-
-                            					<form id="delete-customer{{ $customer->id }}" action="/customers/{{ $customer->id }}" method="POST" style="display: none;">
-                            					    {{ csrf_field() }}
-                            					    {{ method_field('DELETE') }}
-                            					</form>
-                            				</div>
-                            			</div>
-                            		</div>
-                            	</div>
-                            </td>
                         </tr>
                     @endforeach
                 </tbody>
